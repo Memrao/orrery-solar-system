86476f33f27ad074140cbb07075f81423e7e3705
raocaptmemrao@gmail.com <raocaptmemrao@gmail.com>
2024-10-06 00:10:30 +0500

    orrer

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Orrery from './components/orrery/orrery'; // Correct import path
+
+const App = () => {
+  return (
+    <div>
+      <Orrery />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default App;
